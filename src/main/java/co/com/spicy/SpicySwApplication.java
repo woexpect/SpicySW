8c64d8b1fb6797dbdfa90fb65ef2a07aa2a58148
woexpect <sebasp95@hotmail.com>
2017-04-08 12:33:07 -0500

    initial commit

---
+++ src/main/java/co/com/spicy/SpicySwApplication.java
@@ -0,0 +1,12 @@
+package co.com.spicy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpicySwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpicySwApplication.class, args);
+	}
+}
