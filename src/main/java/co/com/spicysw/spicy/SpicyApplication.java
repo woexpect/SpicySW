3cad55bcfabefcfc292ee45f3c7531d642ba9efb
woexpect <sebasp95@hotmail.com>
2017-07-19 10:43:19 -0500

    ajustes nuevo spicy

---
+++ src/main/java/co/com/spicysw/spicy/SpicyApplication.java
@@ -0,0 +1,12 @@
+package co.com.spicysw.spicy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpicyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpicyApplication.class, args);
+	}
+}
